4d9c1bafe2b3fb5bb01da69bfe2595f1d73f49f5
aysegulgules <159700271+aysegulgules@users.noreply.github.com>
2024-09-10 07:37:02 +0300

    Add project files.

---
+++ PatikaWeek8Homework2/Controllers/CustomerOrdersController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PatikaWeek8Homework2.Models;
+
+namespace PatikaWeek8Homework2.Controllers
+{
+    public class CustomerOrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Model nesnesi oluşturuluyo...
+            CustomerOrderViewModel customerOrderViewModel = new CustomerOrderViewModel();
+
+            Customer customer = new Customer()
+            {
+                Id = 1,
+                FirstName = "Pelin",
+                LastName = "Şahin",
+                Email = "psah@dd.com",
+
+            };
+
+            customerOrderViewModel.customer = customer;
+
+            Order order = new Order()
+            {
+                Id = 1,
+                ProductName = "Bilgisayar",
+                Price = 50000,
+                Quantity = 1,
+                
+            };
+            Order order2 = new Order();
+            order2.Id = 2;
+            order2.ProductName = "Telefon";
+            order2.Price = 30000;
+            order2.Quantity = 1;
+
+            customerOrderViewModel.orders=new List<Order> { order, order2 };
+
+          
+
+            //Model view gönderiliyor
+            return View(customerOrderViewModel);
+        }
+    }
+}
---
+++ PatikaWeek8Homework2/Models/CustomerOrderViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PatikaWeek8Homework2.Models
+{
+    public class CustomerOrderViewModel
+    {
+        public Customer customer { get; set; }
+        public List<Order> orders { get; set; }
+    }
+}
